19b5c038c2d77ab03004722604cdf7ddfd1c02f5
Ashish <ashish@norteastern.edu>
2021-02-10 10:29:38 +0530

    Checking in code for Get Stock values

---
+++ GetStockValues/GetStockValues/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  GetStockValues
+//
+//  Created by Ashish Ashish on 10/02/21.
+//
+
+import Foundation
+
+let apiKey = "0ae8565c4294fa80d71b17b3ed6aca19"
+let apiURL = "https://financialmodelingprep.com/api/v3/quote-short/"
---
+++ GetStockValues/GetStockValues/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  GetStockValues
+//
+//  Created by Ashish Ashish on 10/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblStockPrice: UILabel!
+    
+    var globalStockTxtField : UITextField?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func getStockAction(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Get Stock Price?", message: "Type in The Symbol", preferredStyle: .alert)
+        
+        let OK = UIAlertAction(title: "OK", style: .default) { (alertAction) in
+            print("OK")
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (alertAction) in
+            print("Cancel")
+        }
+        
+        alert.addTextField { (stockTxtField) in
+            stockTxtField.placeholder = "Type Stock Symbol"
+            self.globalStockTxtField = stockTxtField
+        }
+        
+        alert.addAction(OK)
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+}
+
