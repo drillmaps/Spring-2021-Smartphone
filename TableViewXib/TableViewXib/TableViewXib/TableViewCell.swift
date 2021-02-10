5cf12dfb9cb15a43b9c8c2e34b77903491840cf9
Ashish <ashish@norteastern.edu>
2021-02-10 09:00:24 +0530

    Commit files

---
+++ TableViewXib/TableViewXib/TableViewXib/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  TableViewXib
+//
+//  Created by Ashish Ashish on 10/02/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var lblCity: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
